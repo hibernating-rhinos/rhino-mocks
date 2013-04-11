5b8983799f3bfba0a053fce4e94321f5598c6ffc
Andreas Ländle <alaendle@freenet.de>
2013-04-11 21:11:39 +0200

    Adapted copyright information for the year 2013.

--- default.ps1
+++ default.ps1
@@ -30,7 +30,7 @@ task Init -depends Clean {
 		-company "Hibernating Rhinos" `
 		-product "Rhino Mocks $version" `
 		-version $version `
-		-copyright "Hibernating Rhinos & Ayende Rahien 2004 - 2012" `
+		-copyright "Hibernating Rhinos & Ayende Rahien 2004 - 2013" `
 		-internalsVisibleTo "Rhino.Mocks.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009d1cf4b75b7218b141ac64c15450141b1e5f41f6a302ac717ab9761fa6ae2c3ee0c354c22d0a60ac59de41fa285d572e7cf33c320aa7ff877e2b7da1792fcc6aa4eb0b4d8294a2f74cb14d03fb9b091f751d6dc49e626d74601692c99eab7718ed76a40c36d39af842be378b677e6e4eae973f643d7065241ad86ecc156d81ab"
 		
 	Generate-Assembly-Info `
@@ -41,7 +41,7 @@ task Init -depends Clean {
 		-product "Rhino Mocks Tests $version" `
 		-version $version `
 		-clsCompliant "false" `
-		-copyright "Hibernating Rhinos & Ayende Rahien 2004 - 2012"
+		-copyright "Hibernating Rhinos & Ayende Rahien 2004 - 2013"
 		
 	Generate-Assembly-Info `
 		-file "$base_dir\Rhino.Mocks.Tests.Model\Properties\AssemblyInfo.cs" `
@@ -51,7 +51,7 @@ task Init -depends Clean {
 		-product "Rhino Mocks Tests Model $version" `
 		-version $version `
 		-clsCompliant "false" `
-		-copyright "Hibernating Rhinos & Ayende Rahien 2004 - 2012"
+		-copyright "Hibernating Rhinos & Ayende Rahien 2004 - 2013"
 	
 	Generate-Assembly-Info `
 		-file "$base_dir\Rhino.Mocks.GettingStarted\Properties\AssemblyInfo.cs" `
@@ -61,7 +61,7 @@ task Init -depends Clean {
 		-product "Rhino Mocks Tests $version" `
 		-version $version `
 		-clsCompliant "false" `
-		-copyright "Hibernating Rhinos & Ayende Rahien 2004 - 2012"
+		-copyright "Hibernating Rhinos & Ayende Rahien 2004 - 2013"
 		
 	new-item $release_dir -itemType directory 
 	new-item $build_dir -itemType directory 
